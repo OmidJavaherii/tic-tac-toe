b3dc47a5f0db69453514bb4a2c0af55a19e6a11a
omid <omidjavaheri.eng@gmail.com>
2025-02-07 21:36:38 +0330

    debug for pwa

--- src/App.js
+++ src/App.js
@@ -10,26 +10,30 @@ export default function App() {
 
   const [deferredPrompt, setDeferredPrompt] = useState(null);
   const [isInstalled, setIsInstalled] = useState(false);
-  
+
   useEffect(() => {
     const checkInstallation = () => {
       const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
       setIsInstalled(isStandalone || window.navigator.standalone);
     };
 
-    window.addEventListener("beforeinstallprompt", (event) => {
+    const handleBeforeInstallPrompt = (event) => {
       event.preventDefault();
       setDeferredPrompt(event);
-    });
+      sessionStorage.setItem("pwaInstallPrompt", true);
+    };
 
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     window.addEventListener("appinstalled", () => {
-      console.log("PWA installed");
+      console.log("PWA Has Installed");
       setIsInstalled(true);
+      sessionStorage.removeItem("pwaInstallPrompt");
     });
 
     checkInstallation();
+
     return () => {
-      window.removeEventListener("beforeinstallprompt", () => { });
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
       window.removeEventListener("appinstalled", () => { });
     };
   }, []);
